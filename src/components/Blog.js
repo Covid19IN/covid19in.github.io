df670a6b026edba5f210e9e068b6d26022fb3989
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-09-08 17:27:07 +0530

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -24,7 +24,7 @@ const Blog = () => {
   return (
 
      <div classname="Blog">
-     {recipes.map(faq => (
+     {recipes.map(recipe => (
       <Recipe
       key={recipe.faq.question}
       title={recipe.faq.question}
