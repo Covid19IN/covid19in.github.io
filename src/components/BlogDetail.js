6eb9ec3fe328eebe7692c20cedf29b376ef7140d
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-09-11 00:45:53 +0530

    Update and rename Blog1.js to BlogDetail.js

---
+++ src/components/BlogDetail.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function BlogDetails({ match }) {
+       useEffect(() => {
+        fetchItem();
+        console.log(match);
+    }, []);
+
+    const [item, setItem] = useState({
+        images: {}
+    });
+
+    const fetchItem = async () => {
+        const fetchItem = await fetch(
+            `https://fortnite-api.com/v2/cosmetics/br/${
+                match.params.id
+            }`
+            );
+        const item = await fetchItem.json();
+        setItem(item);
+        console.log(item);
+    };
+
+  return (
+    <div>
+        <h1>{item.name}</h1>
+        <img src={item.images.transparent} alt="" />
+    </div>
+  );
+}
+
+export default BlogDetail;
