7e930fd86ae65856bdaea374ea2cf711cfd11e6d
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-09-10 15:27:04 +0530

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -47,7 +47,7 @@ function Blog() {
               key={index}
               className="faq fadeInUp"
               style={{animationDelay: `${0.5 + index * 0.1}s`}}
-            >  <img className={`countryCard__flagImg countryCard__flagImg--${country.alpha3Code}`} src={faq.image} alt={faq.question} />
+            >  <img src={faq.image} alt={faq.question} />
               <h2 className="answer">{faq.question}</h2>
 
                <h2
